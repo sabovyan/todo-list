36c6bebd06f48961744bb44c24c0adf822c16fd5
sabovyan <abovyan.sargis90@gmail.com>
2020-10-21 11:19:54 +0400

    ci(server) : split conrollers and routes

---
+++ controllers/todo.controllers.js
@@ -0,0 +1,64 @@
+let todos = [
+  {
+    id: 'todo_1',
+    value: 'go to school',
+    isEdit: false,
+  },
+  {
+    id: 'todo_2',
+    value: 'discover universe',
+    isEdit: false,
+  },
+  {
+    id: 'todo_3',
+    value: 'create a new planet',
+    isEdit: false,
+  },
+];
+
+exports.read = (req, res) => {
+  const response = {
+    status: 'success',
+    data: todos,
+  };
+  res.status(200).send(response);
+};
+exports.create = (req, res) => {
+  todos.push(req.body);
+  const response = {
+    status: 'success',
+    data: todos,
+  };
+  res.status(200).send(response);
+};
+
+exports.update = (req, res) => {
+  const { id } = req.params;
+  if (JSON.stringify(req.body) === JSON.stringify({})) {
+    todos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isEdit: true } : todo
+    );
+  } else {
+    const { value } = req.body;
+    const { isEdit } = req.body;
+
+    todos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isEdit, value } : todo
+    );
+  }
+  const response = {
+    status: 'success',
+    data: todos,
+  };
+  res.status(200).json(response);
+};
+
+exports.remove = (req, res) => {
+  const { id } = req.params;
+  todos = todos.filter((todo) => todo.id !== id);
+  const response = {
+    status: 'success',
+    data: {},
+  };
+  res.status(200).json(response);
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,71 +1,15 @@
 const express = require('express');
+const {
+  create,
+  read,
+  update,
+  remove,
+} = require('../controllers/todo.controllers');
 
 const router = express.Router();
 
-let todos = [
-  {
-    id: 'a1',
-    value: 'go to school',
-    isEdit: false,
-  },
-  {
-    id: 'a2',
-    value: 'discover universe',
-    isEdit: false,
-  },
-  {
-    id: 'a3',
-    value: 'create a new planet',
-    isEdit: false,
-  },
-];
+router.route('/').get(read).post(create);
 
-router.get('/', (req, res) => {
-  const response = {
-    status: 'success',
-    data: todos,
-  };
-  res.status(200).send(response);
-});
-
-router.post('/', (req, res) => {
-  todos.push(req.body);
-  const response = {
-    status: 'success',
-    data: todos,
-  };
-  res.status(200).send(response);
-});
-
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  if (JSON.stringify(req.body) === JSON.stringify({})) {
-    todos = todos.map((todo) =>
-      todo.id === id ? { ...todo, isEdit: true } : todo
-    );
-  } else {
-    const { value } = req.body;
-    const { isEdit } = req.body;
-
-    todos = todos.map((todo) =>
-      todo.id === id ? { ...todo, isEdit, value } : todo
-    );
-  }
-  const response = {
-    status: 'success',
-    data: todos,
-  };
-  res.status(200).json(response);
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  todos = todos.filter((todo) => todo.id !== id);
-  const response = {
-    status: 'success',
-    data: {},
-  };
-  res.status(200).json(response);
-});
+router.route('/:id').put(update).delete(remove);
 
 module.exports = router;
